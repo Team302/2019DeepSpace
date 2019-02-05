a8da51824b47df161086f8669f20fd9a744890b2
Joe Witcpalek <joe.witcpalek@gmail.com>
2019-02-05 18:22:22 -0500

    Add files via upload

--- include/xmlhw/DigitalInputDefn.h
+++ include/xmlhw/DigitalInputDefn.h
@@ -14,7 +14,7 @@
 // FRC includes
 
 // Team 302 includes
-//#include <hw/DragonDigitalInput.h>
+#include <hw/DragonDigitalInput.h>
 
 // Third Party includes
 #include <pugixml/pugixml.hpp>
@@ -35,21 +35,18 @@ class DigitalInputDefn
         //        enum DIGITAL_INPUT_TYPE
         //        {
         //            UNKNOWN_DIGITAL_INPUT_TYPE = -1,
-        //            CUBE_PRESENT,
-        //            ELEVATOR_POSITION_0,
-        //            ELEVATOR_POSITION_1,
-        //            ELEVATOR_POSITION_2,
-        //            ELEVATOR_POSITION_3,
-        //            ELEVATOR_POSITION_4,
-        //            ELEVATOR_POSITION_5,
-        //            ELEVATOR_POSITION_6,
+        //            CARGO_PRESENT,
+        //            HATCH_PRESENT_SIDE1,
+        //            HATCH_PRESENT_SIDE2,
+        //            ARM_MIN_POSITION,
+        //            ARM_MAX_POSITION,
         //            MAX_DIGITAL_INPPUT_TYPES
         //        };
         //
         //     ==================================================== -->
         //<!ELEMENT digitalInput EMPTY>
         //<!ATTLIST digitalInput
-        //          usage             (  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 ) "0"
+        //          usage             (  0 |  1 |  2 |  3 |  4 ) "0"
         //          digitalId         (  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |
         //                              10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 |
         //                              20 | 21 | 22 | 23 | 24 | 25 ) "0"
@@ -59,8 +56,7 @@ class DigitalInputDefn
         //
         // Returns:     DragonDigitalInput*
         //-----------------------------------------------------------------------
-        //static DragonDigitalInput* ParseXML
-        static void ParseXML
+        static DragonDigitalInput* ParseXML
         (
             pugi::xml_node      motorNode
         );
