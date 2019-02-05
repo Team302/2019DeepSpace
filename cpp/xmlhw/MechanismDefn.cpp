e98c980293104b712775343b4bf0e357503927b1
Joe Witcpalek <joe.witcpalek@gmail.com>
2019-02-05 17:55:21 -0500

    Add files via upload

--- cpp/xmlhw/MechanismDefn.cpp
+++ cpp/xmlhw/MechanismDefn.cpp
@@ -10,20 +10,18 @@
 #include <frc/SmartDashboard/SmartDashboard.h>
 
 // Team 302 includes
-//#include <subsys/MechanismFactory.h>
-//#include <subsys/IMechanism.h>
+#include <subsys/MechanismFactory.h>
+#include <subsys/IMechanism.h>
 
-//#include <xmlhw/AnalogInputDefn.h>
+#include <xmlhw/AnalogInputDefn.h>
 #include <xmlhw/DigitalInputDefn.h>
 #include <xmlhw/MotorDefn.h>
-//#include <xmlhw/ServoDefn.h>
-//#include <xmlhw/SolenoidDefn.h>
+//#include <xmlhw/ServoDefn.h>  Chris is working on this
 
-//#include <hw/DragonAnalogInput.h>
-//#include <hw/DragonDigitalInput.h>
-//#include <hw/DragonServo.h>
-//#include <hw/DragonSolenoid.h>
-//#include <hw/DragonTalon.h>
+#include <hw/DragonAnalogInput.h>
+#include <hw/DragonDigitalInput.h>
+#include <hw/DragonServo.h>
+#include <hw/IDragonMotorController.h>
 
 #include <xmlhw/MechanismDefn.h>
 
@@ -43,16 +41,16 @@ using namespace frc;
 //        enum MECHANISM_TYPE
 //        {
 //            UNKNOWN_MECHANISM = -1,
-//            GRABBER,
-//            LIFT,
+//            WRIST,
+//            INTAKE,
+//            ARM,
 //            CLIMBER,
-//            SIDE_HANGER,
 //            MAX_MECHANISM_TYPES
 //        };
 //
 //
 //    ==================================================== -->
-//<!ELEMENT mechanism (motor*, analogInput*, digitalInput*, solenoid*, servo* )>
+//<!ELEMENT mechanism (motor*, analogInput*, digitalInput*, servo* )>
 //<!ATTLIST mechanism
 //          type              ( 0 | 1 | 2 | 3 ) "0"
 //>
@@ -66,7 +64,7 @@ void MechanismDefn::ParseXML
 )
 {
     // initialize attributes
-    //IMechanism::MECHANISM_TYPE type = IMechanism::UNKNOWN_MECHANISM;
+    IMechanism::MECHANISM_TYPE type = IMechanism::UNKNOWN_MECHANISM;
 
     bool hasError       = false;
 
@@ -76,26 +74,22 @@ void MechanismDefn::ParseXML
         if ( strcmp( attr.name(), "type" ) == 0 )
         {
             int iVal = attr.as_int();
-            /*switch ( iVal )
+            switch ( iVal )
             {
-                case IMechanism::GRABBER:
-                    type = IMechanism::GRABBER;
+                case IMechanism::WRIST:
+                    type = IMechanism::WRIST;
                     break;
 
-                case IMechanism::LIFT:
-                    type = IMechanism::LIFT;
+                case IMechanism::INTAKE:
+                    type = IMechanism::INTAKE;
+                    break;
+
+                case IMechanism::ARM:
+                    type = IMechanism::ARM;
                     break;
 
                 case IMechanism::CLIMBER:
                     type = IMechanism::CLIMBER;
-                    break;
-
-                case IMechanism::SIDE_HANGER:
-                    type = IMechanism::SIDE_HANGER;
-                    break;
-
-                case IMechanism::ACTIVE_GRABBER:
-                    type = IMechanism::ACTIVE_GRABBER;
                     break;
 
                 default:
@@ -103,27 +97,24 @@ void MechanismDefn::ParseXML
                     hasError = true;
                     break;
                 
-            }*/
+            }
         }
         else
         {
-            //printf( "==>>MechanismDefn::ParseXML invalid attribute %s \n", attr.name() );
+            printf( "==>>MechanismDefn::ParseXML invalid attribute %s \n", attr.name() );
             hasError = true;
         }
     }
 
     // initialize the subobjects
-    //DragonTalonVector motors;
-    //DragonAnalogInputVector analogIns;
-    //DragonDigitalInputVector digitals;
-    //DragonSolenoidVector solenoids;
-    //DragonServoVector servos;
+    IDragonMotorControllerVector motors;
+    DragonAnalogInputVector analogIns;
+    DragonDigitalInputVector digitals;
+    DragonServoVector servos;
 
     // Parse/validate subobject xml
-    //for (pugi::xml_node child = mechanismNode.first_child(); child; child = child.next_sibling())
+    for (pugi::xml_node child = mechanismNode.first_child(); child; child = child.next_sibling())
     {
-        /*    	printf(child.name());
-            	printf("\n");
         if ( strcmp( child.name(), "motor") == 0 )
         {
             motors.emplace_back( MotorDefn::ParseXML( child ) );
@@ -134,34 +125,27 @@ void MechanismDefn::ParseXML
         }
         else if ( strcmp( child.name(), "digitalInput") == 0 )
         {
-            //        	printf("!!!!!!!!!!!!!!!!!!!!!!!1 IM HERE RIGHT NOWWW1!!!JONAH IS HREREE\n");
             digitals.emplace_back( DigitalInputDefn::ParseXML( child ) );
-        }
-        else if ( strcmp( child.name(), "solenoid") == 0 )
-        {
-            solenoids.emplace_back( SolenoidDefn::ParseXML( child ) );
         }
         else if ( strcmp( child.name(), "servo") == 0 )
         {
-            servos.emplace_back( ServoDefn::ParseXML( child ) );
+//            servos.emplace_back( ServoDefn::ParseXML( child ) );
         }
         else
         {
             printf( "==>> MechanismDefn::ParseXML unknown mechanism child %s \n", child.name() );
-        }*/
+        }
     }
 
 
     // create instance
-    /*if ( !hasError )
+    if ( !hasError )
     {
         MechanismFactory* factory =  MechanismFactory::GetMechanismFactory();
         factory->CreateMechanism( type,                     // mechanism type
                                   motors,                   // dragon talons
-                                  solenoids,                // dragon solenoids
                                   digitals,                 // dragon digital inputs
                                   analogIns,                // dragon analog inputs
                                   servos );                 // dragon servos
-    }*/
+    }
 }
-
