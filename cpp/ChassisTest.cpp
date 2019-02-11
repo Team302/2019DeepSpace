2b72d409f2a64ba92bb2bd25d612122ae4fa51a7
ajgdowski <logdowskia56@stu.lok12.org>
2019-02-11 18:38:04 -0500

    Update ChassisTest.cpp

--- cpp/ChassisTest.cpp
+++ cpp/ChassisTest.cpp
@@ -11,18 +11,32 @@ ChassisTest::ChassisTest ()
 
 void ChassisTest::Init ()
 {
-    
+    m_ChassisCounts = 0;
+
+    m_isDone = false;
 }
 
-void ChassisTest::Execute ()
+void ChassisTest::Execute (CHASSIS_PRESETS)
 {
     SetLeftRightMagnitudes(.5, .5);
-    
-}
+    if (SetLeftRightMagnitudes(.5, .5))
+    {
+        m_ChassisCounts++;
+    }
 
+    if (m_ChassisCounts == 1500)
+    {
+        m_isDone = true;
+    }
+}
+//TODO #1 Use enum to set speeds based on what direction we need to move
 bool ChassisTest::IsDone ()
 {
-
+    if (m_isDone = true)
+    {
+        SetLeftRightMagnitudes(0, 0);
+        printf ("Automated Systems Test Complete \n");
+    }
 }
 
 
