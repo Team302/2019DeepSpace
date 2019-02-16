a9d7a76c4331f0012b6f2af2e1039cc8a4e2190e
ajgdowski <logdowskia56@stu.lok12.org>
2019-02-15 21:39:18 -0500

    Update ChassisTest.cpp

--- cpp/ChassisTest.cpp
+++ cpp/ChassisTest.cpp
@@ -23,6 +23,7 @@ void ChassisTest::Init (CHASSIS_PRESETS)
 
     double left = 0.0;
     double right = 0.0;
+    double m_targetCounts = 6000;
     m_chassisPresets = CHASSIS_PRESETS;
 }
 
@@ -33,21 +34,25 @@ void ChassisTest::Execute ()
         case CHASSIS_FORWARDS :
             left = .5;
             right = .5;
+            m_targetCounts = 6000;
             break;
 
         case CHASSIS_BACKWARDS :
             left = -.5;
             right = -.5;
+            m_targetCounts = 6000;
             break;
 
         case CHASSIS_LEFT : 
             left = -.5;
             right = .5;
+            m_targetCounts = 6000;
             break;
 
         case CHASSIS_RIGHT :
             left = .5;
             right = -.5;
+            m_targetCounts = 6000;
             break;
 
         case CHASSIS_STOP :
@@ -60,42 +65,6 @@ void ChassisTest::Execute ()
     }
     SetLeftRightMagnitudes(left, right);
 
-    if (SetLeftRightMagnitudes(.5, .5))
-    {
-        m_forwardCounts++;
-        GetDistance((left+right) /2);
-        if (m_forwardCounts == 1500);
-        {
-            printf("Driving Forwards complete \n");
-        }
-    }
-    else if (SetLeftRightMagnitudes(-.5, -.5))
-    {
-        m_backwardsCounts++;
-        GetDistance((left+right) /2);
-        if (m_backwardsCounts == 1500);
-        {
-            printf("Driving Backwards complete \n");
-        }
-    }
-    else if (SetLeftRightMagnitudes(-.5, .5))
-    {
-        m_leftCounts++;
-        GetDistance((left+right) /2);
-        if (m_leftCounts == 1500);
-        {
-            printf("Driving Left complete \n");
-        }
-    }
-    else if (SetLeftRightMagnitudes(.5, -.5))
-    {
-        m_rightCounts++;
-        GetDistance((left+right) /2);
-        if (m_rightCounts == 1500);
-        {
-            printf("Driving Right complete \n");
-        }
-    }
 }
 //TODO #1 Use enum to set speeds based on what direction we need to move
 bool ChassisTest::IsDone ()
