14a9c51f5b2228538e650b728e2999eb75ca5f10
AJ Gdowski <45185551+ajgdowski@users.noreply.github.com>
2019-03-13 18:55:50 -0400

    Add files via upload

---
+++ ChassisTest.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+#include <frc/timedrobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+
+#include "subsys/chassis/DragonChassis.h"
+
+class ChassisTest {
+    public:
+    ChassisTest();
+    void Init();
+    void Execute();
+    bool IsDone();
+    enum CHASSIS_PRESETS
+    {
+        CHASSIS_FORWARDS,
+        CHASSIS_BACKWARDS,
+        CHASSIS_LEFT,
+        CHASSIS_RIGHT,
+        CHASSIS_STOP,
+    };
+
+    private:
+    DragonChassis* m_chassis;
+    int m_ChassisCounts = 0;
+    bool m_isDone = false;
+
+}
